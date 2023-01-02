a73f3bbb88677ec98906ed03f913a396a63bb611
beygee <doug0476@naver.com>
2023-01-02 16:06:17 +0900

    fix: icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
-import { IconContext } from 'react-icons'
+import { IconContext } from 'react-icons/lib'
 
 import { renderStatic } from '@/lib/renderer'
 
